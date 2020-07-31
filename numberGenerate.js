da17606d629ddd2f20e1ebf2c23905fb8753e359
wesink <35277359+wesink@users.noreply.github.com>
2020-07-31 11:38:52 -0700

    Add files via upload

---
+++ numberGenerate.js
@@ -0,0 +1,4 @@
+alert("Please click OK to generate a number number between 1-100");
+var numberRandom = Math.random() * 100;
+numberRandom = Math.floor(numberRandom) + 1;
+alert("Your random number out of 100 is " + numberRandom);
